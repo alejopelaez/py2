fdd00c8fc8f386fa1fa919372cc83517dbc525e2
Alejandro Peláez <alejopelaezr@gmail.com>
2010-11-28 16:54:22 -0500

    Resetting

--- app/helpers/materias_helper.rb
+++
@@ -1,2 +0,0 @@
-module MateriasHelper
-end
--- db/migrate/20101128171548_community_engine_to_version_73.rb
+++
@@ -1,9 +0,0 @@
-class CommunityEngineToVersion73 < ActiveRecord::Migration
-  def self.up
-    migrate_plugin(:community_engine, 73)  
-  end
-
-  def self.down
-    migrate_plugin(:community_engine, 0)    
-  end
-end
