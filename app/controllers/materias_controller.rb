d52a179edb68df20477993a12a78f783e6a5aab2
Alejandro Peláez <alejopelaezr@gmail.com>
2010-11-28 23:41:13 -0500

    Getting the subjects from an external xml

--- app/controllers/materias_controller.rb
+++ app/controllers/materias_controller.rb
@@ -1,3 +1,6 @@
+require 'rexml/document'
+include REXML
+
 class MateriasController < BaseController
   def index
   end
@@ -13,4 +16,28 @@ class MateriasController < BaseController
   def load_from_ws
   end
 
+  def load_from_xml
+    begin
+      @materias = Materia.load_from_xml
+
+      @materias.elements.each("materias/materia") do |m|
+        materia = Materia.find_by_code(m.attributes["codigo"])
+        materia = Materia.new if(materia == nil)
+        materia.name =        m.attributes["nombre"]
+        materia.code =        m.attributes["codigo"]
+        materia.credits =     m.attributes["creditos"]
+        materia.umes =        m.attributes["umes"]
+        materia.professor =   m.elements["profesor"].attributes["nombre"]
+        materia.description = m.elements["descripcion"].text
+        materia.homepage =    m.elements["homepage"].attributes["url"]
+        materia.program =     m.elements["programa"].attributes["url"]
+
+        materia.save
+      end
+
+    rescue Exception => e
+      puts "Error ---- #{e.message} ------"
+    end
+    redirect_to :action => :list
+  end
 end
--- app/models/materia.rb
+++ app/models/materia.rb
@@ -1,2 +1,12 @@
+require 'rexml/document'
+include REXML
+
 class Materia < ActiveRecord::Base
+
+    validates_uniqueness_of :code
+
+  def self.load_from_xml
+    Document.new(File.new("public/materias.xml"))
+  end
+
 end
